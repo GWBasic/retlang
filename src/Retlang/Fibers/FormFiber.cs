17b2e3d20aad93fe9d31badada51aec503bb0ebe
anthony taranto <anthony.taranto@gmail.com>
2012-11-21 01:35:05 -0800

    Revert "remove forms/dispatcher specific classes, remove extraneous references"
    This reverts commit 9146a7f1dacd3cd57e3726cdfb9233aae6882f2c.

---
+++ src/Retlang/Fibers/FormFiber.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Retlang.Core;
+
+namespace Retlang.Fibers
+{
+    ///<summary>
+    /// Allows interaction with Windows Forms.  Transparently moves actions onto the Form's thread.
+    ///</summary>
+    public class FormFiber : GuiFiber
+    {
+        /// <summary>
+        /// Creates an instance.
+        /// </summary>
+        public FormFiber(ISynchronizeInvoke invoker, IExecutor executor)
+            : base(new FormAdapter(invoker), executor)
+        {
+        }
+    }
+}
