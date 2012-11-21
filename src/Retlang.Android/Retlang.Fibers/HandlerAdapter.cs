335d0f4d9bbceed3c7763e6a1ab4ac2fb6d92a01
anthony taranto <anthony.taranto@gmail.com>
2012-11-21 01:59:37 -0800

    add Retlang.Android.csproj

---
+++ src/Retlang.Android/Retlang.Fibers/HandlerAdapter.cs
@@ -0,0 +1,22 @@
+using System;
+using Retlang.Core;
+using Android.OS;
+
+namespace Retlang.Fibers
+{
+	public class HandlerAdapter : IExecutionContext
+	{
+		private readonly Handler _handler;
+
+		public HandlerAdapter (Handler handler)
+		{
+			_handler = handler;
+		}
+
+		public void Enqueue (Action action)
+		{
+			_handler.Post(action);
+		}
+	}
+}
+
---
+++ src/Retlang.Android/Retlang.Fibers/HandlerFiber.cs
@@ -0,0 +1,14 @@
+using Retlang.Core;
+using Android.OS;
+
+namespace Retlang.Fibers
+{
+	public class HandlerFiber : GuiFiber
+	{
+		public HandlerFiber (Handler handler, IExecutor executor)
+			: base(new HandlerAdapter(handler), executor)
+		{
+		}
+	}
+}
+
