4b0177d26c12f180026cab46a597f531f13ad3e8
anthony taranto <anthony.taranto@gmail.com>
2013-01-02 17:02:36 -0800

    add DebounceReceiver, update receiver tests to use PoolFiber

--- src/Retlang.Test/Retlang.Test.Channels/DebounceReceiverFixture.cs
+++ src/Retlang.Test/Retlang.Test.Channels/DebounceReceiverFixture.cs
@@ -8,27 +8,27 @@ using Retlang.Fibers;
 namespace Retlang.Test.Channels
 {
     [TestFixture]
-    public class LastReceiverFixture : MoqTestFixture
+    public class DebounceReceiverFixture : MoqTestFixture
     {
         private IList<int> _received;
         private AutoResetEvent _handle;
         private IFiber _fiber;
-        private LastReceiver<int> _receiver;
-        
+        private DebounceReceiver<int> _receiver;
+
         protected override void SetUp()
         {
             _received = new List<int>();
             _handle = new AutoResetEvent(false);
             _fiber = new PoolFiber();
             _fiber.Start();
-            
-            _receiver = new LastReceiver<int>(_fiber, x =>
+
+            _receiver = new DebounceReceiver<int>(_fiber, x =>
             {
                 _received.Add(x);
                 _handle.Set();
             }, 50);
         }
-        
+
         [Test]
         public void Receive_OneMessage_ReceivesMessage()
         {
@@ -39,23 +39,19 @@ namespace Retlang.Test.Channels
             Assert.AreEqual(1, _received.Count);
             Assert.AreEqual(0, _received[0]);
         }
-        
+
         [Test]
-        public void Receive_MultipleMessages_ReceivesLastMessagePerInterval()
+        public void Receive_MultipleMessages_ReceivesLastMessage()
         {
             for (var x = 0; x < 10; x++)
             {
                 _receiver.Receive(x);
-                
-                if (x % 2 == 1)
-                {
-                    var signaled = _handle.WaitOne(1000);
-                    Assert.IsTrue(signaled);
-                }
             }
-            
-            Assert.AreEqual(5, _received.Count);
-            Assert.AreEqual(new int[] { 1, 3, 5, 7, 9 }, _received);
+
+            var signaled = _handle.WaitOne(1000);
+            Assert.IsTrue(signaled);
+            Assert.AreEqual(1, _received.Count);
+            Assert.AreEqual(9, _received[0]);
         }
     }
 }
---
+++ src/Retlang/Retlang.Channels/DebounceReceiver.cs
@@ -0,0 +1,68 @@
+using System;
+using Retlang.Core;
+using Retlang.Fibers;
+
+namespace Retlang.Channels
+{
+    public static class DebounceReceiverExtensions
+    {
+        public static IDisposable SubscribeToDebounce<T>(this ISubscriber<T> subscriber,
+            IFiber fiber, Action<T> receive, long intervalInMs)
+        {
+            var receiver = new DebounceReceiver<T>(fiber, receive, intervalInMs);
+            return subscriber.Subscribe(receiver);
+        }
+    }
+    
+    /// <summary>
+    /// Receives the last action received on the channel before a period of inactivity.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DebounceReceiver<T> : BaseReceiver<T>
+    {
+        private readonly object _lock = new object();
+
+        private readonly Action<T> _receive;
+        private readonly long _intervalInMs;
+
+        private IDisposable _scheduled;
+        private T _pending;
+
+        public DebounceReceiver(IFiber fiber, Action<T> receive, long intervalInMs)
+            : base(fiber)
+        {
+            _receive = receive;
+            _intervalInMs = intervalInMs;
+        }
+
+        protected override void ReceiveFiltered(T msg)
+        {
+            lock (_lock)
+            {
+                _pending = msg;
+
+                if (_scheduled != null)
+                {
+                    _scheduled.Dispose();
+                }
+                _scheduled = _fiber.Schedule(Flush, _intervalInMs);
+            }
+        }
+
+        private void Flush()
+        {
+            T message;
+            lock (_lock)
+            {
+                message = _pending;
+                _pending = default(T);
+
+                _scheduled.Dispose();
+                _scheduled = null;
+            }
+
+            _receive(message);
+        }
+    }
+}
+
