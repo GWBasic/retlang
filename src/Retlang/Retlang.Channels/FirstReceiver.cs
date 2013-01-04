0c9ac7fc3b210e039341c9ab4685421265cc75b0
anthony taranto <anthony.taranto@gmail.com>
2013-01-04 02:15:02 -0800

    FirstReceiver: add convenience extension methods

--- src/Retlang/Retlang.Channels/FirstReceiver.cs
+++ src/Retlang/Retlang.Channels/FirstReceiver.cs
@@ -3,6 +3,30 @@ using Retlang.Fibers;
 
 namespace Retlang.Channels
 {
+    public static class FirstReceiverExtensions
+    {
+        // XXX: SubscribeToFirstThenThrottle
+        public static IDisposable SubscribeToFirstThenLast<T>(this ISubscriber<T> subscriber,
+            IFiber fiber, Action<T> receive, long intervalInMs)
+        {
+            var first = new Receiver<T>(fiber, receive);
+            var rest = new LastReceiver<T>(fiber, receive, intervalInMs);
+
+            var receiver = new FirstReceiver<T>(fiber, first, rest);
+            return subscriber.Subscribe(receiver);
+        }
+
+        public static IDisposable SubscribeToFirstThenDebounce<T>(this ISubscriber<T> subscriber,
+            IFiber fiber, Action<T> receive, long intervalInMs)
+        {
+            var first = new Receiver<T>(fiber, receive);
+            var rest = new DebounceReceiver<T>(fiber, receive, intervalInMs);
+
+            var receiver = new FirstReceiver<T>(fiber, first, rest);
+            return subscriber.Subscribe(receiver);
+        }
+    }
+
     public class FirstReceiver<T> : BaseReceiver<T>
     {
         private IReceiver<T> _first;
@@ -11,6 +35,11 @@ namespace Retlang.Channels
         public FirstReceiver(IFiber fiber, IReceiver<T> first, IReceiver<T> rest)
             : base(fiber)
         {
+            if (first.Subscriptions != rest.Subscriptions)
+            {
+                // XXX: maybe this is bad?
+            }
+
             _first = first;
             _rest = rest;
         }
